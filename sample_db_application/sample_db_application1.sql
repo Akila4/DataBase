61ff978c57afaad985beb0bac1d7ccd208620281
Akila4 <32973903+Akila4@users.noreply.github.com>
2019-02-12 11:51:31 +0530

    Add files via upload

---
+++ sample_db_application/sample_db_application1.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE dbapplication1;
+use dbapplication1;
+CREATE TABLE users_data1(id int primary key auto_increment, name varchar(40), email varchar(50), phone varchar(20));
+INSERT INTO users_data1(name, email, phone) values("user1", "user1@gmail.com", "8678904323");
