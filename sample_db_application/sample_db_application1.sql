493a75ed1b04e466ec437acf6f366fbd840930ef
AKILA M (MAS) <AK20023902@wipro.com>
2019-02-12 13:19:43 +0530

    third trial

--- sample_db_application/sample_db_application1.sql
+++ sample_db_application/sample_db_application1.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE dbapplication1;
 use dbapplication1;
 CREATE TABLE users_data1(id int primary key auto_increment, name varchar(40), email varchar(50), phone varchar(20));
-INSERT INTO users_data1(name, email, phone) values("user1", "user1@gmail.com", "8678904323");
-INSERT INTO users_data1(name, email, phone) values("user2", "user2@gmail.com", "8654904322");
+INSERT INTO users_data1(name, email, phone) values("user1", "user1@gmail.com", "8678905323");
+INSERT INTO users_data1(name, email, phone) values("user2", "user2@gmail.com", "8650904322");
